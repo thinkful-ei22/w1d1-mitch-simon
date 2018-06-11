4d2b5e4fe78b9b80d864c2cfeeb6670f26cbd660
Simon Chae <s13chae@gmail.com>
2018-06-11 13:02:35 -0700

    Cracking the code

--- drills/drills-two.js
+++ drills/drills-two.js
@@ -8,19 +8,41 @@
 
 // jediName('John', 'Dude');
 
-function beyond(num) {
-  if (num === Infinity){
-    console.log('And beyond');
-  }
-  else if (num !== Infinity && num > 0) {
-    console.log('To infinity');
-  }
-  else if (num !== Infinity && num < 0) {
-    console.log('To negative infinity');
-  }
-  else if (num === 0) {
-    console.log('Staying home');
-  }
+// function beyond(num) {
+//   if (num === Infinity){
+//     console.log('And beyond');
+//   }
+//   else if (num !== Infinity && num > 0) {
+//     console.log('To infinity');
+//   }
+//   else if (num !== Infinity && num < 0) {
+//     console.log('To negative infinity');
+//   }
+//   else if (num === 0) {
+//     console.log('Staying home');
+//   }
+// }
+
+// beyond(Infinity);
+
+function decode(str) {
+	let wordArray = str.toLowerCase().split('');
+	if (wordArray[0] === 'a') {
+		return wordArray[1];
+	} else if (wordArray[0] === 'b') {
+		return wordArray[2];
+	} else if (wordArray[0] === 'c') {
+		return wordArray[3];
+	} else if (wordArray[0] === 'd') {
+		return wordArray[4];
+	} else {
+		return ' ';
+	}
 }
 
-beyond(Infinity);
+console.log(decode('craft') + decode('block') + decode('argon') + decode('meter') + decode('bells') + decode('brown') + decode('croon') + decode('droop'));
+
+
+
+
+
