e18b101573defdceca8b3e72155846db2c84ef3a
Simon Chae <s13chae@gmail.com>
2018-06-11 13:36:11 -0700

    Rock Paper Scissors

--- drills/drills-two.js
+++ drills/drills-two.js
@@ -42,40 +42,74 @@
 
 // console.log(decode('craft') + decode('block') + decode('argon') + decode('meter') + decode('bells') + decode('brown') + decode('croon') + decode('droop'));
 
-function daysInMonth (month, leapYear) {
-  let days = 0;
+// function daysInMonth (month, leapYear) {
+//   let days = 0;
 
-  switch(month) {
-    case 'January':
-    case 'March':
-    case 'May':
-    case 'July':
-    case 'August':
-    case 'October':
-    case 'December':
-      days = 31;
-      break;
-    case 'April':
-    case 'June':
-    case 'September':
-    case 'November':
-      days = 30;
-      break;
-    case 'February':
-      if (leapYear) {
-        days = 29;
-      }
-      else {
-        days = 28;
-      }
-      break;
-    default:
-      throw new Error('Must provide a valid month.');
+//   switch(month) {
+//     case 'January':
+//     case 'March':
+//     case 'May':
+//     case 'July':
+//     case 'August':
+//     case 'October':
+//     case 'December':
+//       days = 31;
+//       break;
+//     case 'April':
+//     case 'June':
+//     case 'September':
+//     case 'November':
+//       days = 30;
+//       break;
+//     case 'February':
+//       if (leapYear) {
+//         days = 29;
+//       }
+//       else {
+//         days = 28;
+//       }
+//       break;
+//     default:
+//       throw new Error('Must provide a valid month.');
+//   }
+
+//   console.log(days);
+// }
+
+
+// daysInMonth('January');
+
+function rockPaperScissors(num) {
+  if (num !== 1 && num !== 2 && num !== 3) {
+    throw new Error('Number must be 1, 2, or 3');
   }
-
-  console.log(days);
+	const randomNo = Math.floor(Math.random() * 3) + 1;
+  console.log(`the computer picked ${randomNo}`);
+  if (num === randomNo) {
+    return 'tie';
+  }
+  switch(num) {
+    case 1:
+      if (randomNo === 2) {
+        return 'lose';
+      } else {
+        return 'win';
+      }
+    case 2:
+      if (randomNo === 3) {
+        return 'lose';
+      } else {
+        return 'win';
+      }
+    case 3:
+      if (randomNo === 1) {
+        return 'lose';
+      } else {
+        return 'win';
+      }
+  }
 }
 
+console.log(rockPaperScissors(3));
 
-daysInMonth('January');
 
