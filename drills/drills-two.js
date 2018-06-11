355c3464f8f25ee0ed603dab52ff5577990a8ff7
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-11 16:16:29 -0400

    How many days in a month

--- drills/drills-two.js
+++ drills/drills-two.js
@@ -25,24 +25,57 @@
 
 // beyond(Infinity);
 
-function decode(str) {
-	let wordArray = str.toLowerCase().split('');
-	if (wordArray[0] === 'a') {
-		return wordArray[1];
-	} else if (wordArray[0] === 'b') {
-		return wordArray[2];
-	} else if (wordArray[0] === 'c') {
-		return wordArray[3];
-	} else if (wordArray[0] === 'd') {
-		return wordArray[4];
-	} else {
-		return ' ';
-	}
+// function decode(str) {
+//   let wordArray = str.toLowerCase().split('');
+//   if (wordArray[0] === 'a') {
+//     return wordArray[1];
+//   } else if (wordArray[0] === 'b') {
+//     return wordArray[2];
+//   } else if (wordArray[0] === 'c') {
+//     return wordArray[3];
+//   } else if (wordArray[0] === 'd') {
+//     return wordArray[4];
+//   } else {
+//     return ' ';
+//   }
+// }
+
+// console.log(decode('craft') + decode('block') + decode('argon') + decode('meter') + decode('bells') + decode('brown') + decode('croon') + decode('droop'));
+
+function daysInMonth (month, leapYear) {
+  let days = 0;
+
+  switch(month) {
+    case 'January':
+    case 'March':
+    case 'May':
+    case 'July':
+    case 'August':
+    case 'October':
+    case 'December':
+      days = 31;
+      break;
+    case 'April':
+    case 'June':
+    case 'September':
+    case 'November':
+      days = 30;
+      break;
+    case 'February':
+      if (leapYear) {
+        days = 29;
+      }
+      else {
+        days = 28;
+      }
+      break;
+    default:
+      throw new Error('Must provide a valid month.');
+  }
+
+  console.log(days);
 }
 
-console.log(decode('craft') + decode('block') + decode('argon') + decode('meter') + decode('bells') + decode('brown') + decode('croon') + decode('droop'));
 
-
-
-
+daysInMonth('January');
 
