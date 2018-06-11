238270de6299ec98a217ba32291860b0b0d99953
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-11 14:39:55 -0400

    fourth exercise complete

--- drills/drills-one.js
+++ drills/drills-one.js
@@ -1,7 +1,9 @@
 'use strict';
 
 function whoAmI(name, age) {
+  var yearOfBirth = 2018 - age;
   console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
+  console.log('I was born in ' + yearOfBirth + '.');
 }
 
 whoAmI('John', 77);
