ce364abcd2c981e324c6bf37cf1ff49ebb92fa3b
MitchGianoni <mitchgianoni@yahoo.com>
2018-06-11 15:45:32 -0400

    To infinity...

--- drills/drills-two.js
+++ drills/drills-two.js
@@ -1,7 +1,26 @@
-function jediName(firstName, lastName) {
-  let splitFirst = firstName.split('');
-  let splitLast = lastName.split('');
-  console.log(splitLast[0] + splitLast[1] + splitLast[2] + splitFirst[0] + splitFirst[1])
+'use strict';
+
+// function jediName(firstName, lastName) {
+//   let splitFirst = firstName.split('');
+//   let splitLast = lastName.split('');
+//   console.log(splitLast[0] + splitLast[1] + splitLast[2] + splitFirst[0] + splitFirst[1])
+// }
+
+// jediName('John', 'Dude');
+
+function beyond(num) {
+  if (num === Infinity){
+    console.log('And beyond');
+  }
+  else if (num !== Infinity && num > 0) {
+    console.log('To infinity');
+  }
+  else if (num !== Infinity && num < 0) {
+    console.log('To negative infinity');
+  }
+  else if (num === 0) {
+    console.log('Staying home');
+  }
 }
 
-jediName('John', 'Dude');
+beyond(Infinity);
