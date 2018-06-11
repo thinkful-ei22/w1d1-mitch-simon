b590883927a28c6ecfd38ed45ed58f54f468c05c
Simon Chae <s13chae@gmail.com>
2018-06-11 11:37:01 -0700

    third exercise complete

--- drills/drills-one.js
+++ drills/drills-one.js
@@ -1,7 +1,7 @@
 'use strict';
 
-function whoAmI() {
-  console.log('Hi, my name is Chris and I\'m 29 years old');
+function whoAmI(name, age) {
+  console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
 }
 
-whoAmI();
+whoAmI('John', 77);
