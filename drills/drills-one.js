1b846b243d17f51bea8b0100f2b7895f94edfe0a
Simon Chae <s13chae@gmail.com>
2018-06-11 11:47:37 -0700

    fifth exercise complete

--- drills/drills-one.js
+++ drills/drills-one.js
@@ -1,9 +1,13 @@
 'use strict';
 
 function whoAmI(name, age) {
-  var yearOfBirth = 2018 - age;
+	var yob = yearOfBirth(age);
   console.log(`Hi, my name is ${name} and I\'m ${age} years old`);
-  console.log('I was born in ' + yearOfBirth + '.');
+  console.log('I was born in ' + yob + '.');
+}
+
+function yearOfBirth(age) {
+	return 2018 - age;
 }
 
 whoAmI('John', 77);
